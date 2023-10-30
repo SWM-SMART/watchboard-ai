3e0c1c131c3699f247d9b50eaab373a1b391f062
minseok-oh <minseok.dev@gmail.com>
2023-10-30 13:18:42 +0900

    ✨ feat: add api endpoints

--- app/api/v1/endpoints/keywords.py
+++ app/api/v1/endpoints/keywords.py
@@ -1,12 +1,9 @@
-from typing import Any
-
 from fastapi import APIRouter, Depends
 
-from app.controller.mindmap import MindMapController
-from app.schemas.mindmap import MindMap
 from app.schemas.document import Document
-
-from app.api.deps import get_mindmap_controller, get_s3_controller
+from app.schemas.context import Keywords
+from app.controller.keywords import KeywordsController
+from app.api.deps import get_keyword_controller, get_s3_controller
 import botocore
 from langchain.document_loaders import PyPDFLoader
 
@@ -17,14 +14,14 @@ from app.core.config import (
 
 router = APIRouter()
 
-@router.post("/mindmap", response_model=None)
-def get_mind_map(
+@router.post("/keywords", response_model=None)
+def get_keywords(
     document: Document, 
-    mindmap_controller: MindMapController = Depends(get_mindmap_controller),
+    keyword_controller: KeywordsController = Depends(get_keyword_controller),
     s3_controller: botocore.client = Depends(get_s3_controller)
-    ) -> MindMap:
-
+    ) -> Keywords:
+    
     s3_controller.download_file(S3_BUCKET_NAME, S3_PREFIX + document.dbInfo.key, f'app/static/{document.dbInfo.key}')
     loader = PyPDFLoader(f'app/static/{document.dbInfo.key}')
     doc = loader.load_and_split()
-    return mindmap_controller.get_mindmap(doc, document.keywords)
+    return keyword_controller.get_keywords(doc)
